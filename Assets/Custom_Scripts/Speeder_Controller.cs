9a6b90d6b0f89b48449c7f24c19a8b5c45d595ff
TensAndTwenties <robbie.carter@gmail.com>
2017-10-22 12:09:56 -0700

    init commit

---
+++ Assets/Custom_Scripts/Speeder_Controller.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Speeder_Controller : MonoBehaviour {
+
+    private KeyCode previouslyPressed = 0;
+    private Rigidbody speederBody;
+    private GameObject speeder;
+    public float torque;
+    public float thrust;
+
+
+	// Use this for initialization
+	void Start () {
+        speeder = GameObject.Find("Speeder");
+        speederBody = speeder.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A)) {
+            
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            //bank left
+            speederBody.AddForce(transform.right * thrust);
+            //Accelerate/continue accelerating
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            //backward accel (mild)
+            //bank right
+            speederBody.AddForce(-transform.right * thrust);
+        }
+
+    }
+}
---
+++ Assets/Custom_Scripts/Suspension.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Suspension : MonoBehaviour {
+
+    public float springForce;
+    public float dampingForce;
+    public float springConstant;
+    public float dampingConstant;
+    public float restLength;
+
+    public Rigidbody rb;
+
+    private float prevLength;
+    private float currentLength;
+    private float springVelocity;
+	// Use this for initialization
+	void Start () {
+        rb = this.gameObject.GetComponentInParent<Rigidbody>();
+        springConstant = rb.mass * 15;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(transform.position, -transform.up, out hit ,restLength)) {
+            prevLength = currentLength;
+            currentLength = restLength - (hit.distance);
+            springVelocity = (currentLength - prevLength) / Time.fixedDeltaTime;
+            springForce = springConstant * currentLength;
+            dampingForce = dampingConstant * springVelocity;
+
+            rb.AddForce(transform.up * (springForce + dampingForce));
+        }
+	}
+}
